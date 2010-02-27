ae65c28893e48ca664d68927d333b6b4da552dc4
jerome.jaglale <jerome.jaglale@c5f109f2-20d2-11df-a5a4-5bcf8f6a2e7e>
2010-02-27 00:05:40 +0000

    minor

--- trunk/color_tool/system/application/views/home.php
+++ trunk/color_tool/system/application/views/home.php
@@ -1,18 +1,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-
-	<title>Color Tool</title>
-	
+	<title>Color Tool</title>	
 </head>
 
 <body>
-
+	
 <h1>Color Tool</h1>
-
 <hr />
 
 <form action="" method="post" accept-charset="utf-8">
