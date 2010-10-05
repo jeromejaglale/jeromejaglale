f6ff2c7caaca30a23741c86f4ede5576e292f58c
jerome.jaglale <jerome.jaglale@c5f109f2-20d2-11df-a5a4-5bcf8f6a2e7e>
2010-10-05 23:25:32 +0000

    cleaning

--- trunk/recover_msn_logs/toto.py
+++ trunk/recover_msn_logs/toto.py
@@ -9,24 +9,26 @@ f = codecs.open(path, 'rU', encoding='latin1')
 for l in f:
 	s += l
 
-i = 0
 for l in s.splitlines(True):
-	i += 1
 	l2 = l.encode('ascii', 'xmlcharrefreplace')
-	l3 = l.encode('ascii', 'replace')
 	l2 = str(l2)
-	for k in l2:
-		pass
-		#print ord(k)
-	#l2 = l2[:6]
+#	for k in l2:
+#		pass
+#		#print ord(k)
+#	#l2 = l2[:6]
+	
+	# if font line, stop	
 	if l2.startswith('Lucida'):
-		print "xxx"
+		print "stopped (Lucida)"
 		break
-	#print "aaaaaaaa"
+		
+	# if weird line, stop
 	l22 = ''.join([str(ord(k)) for k in l2])
 	if l22.startswith('0383549544859038354954485903835495448590400102041'):
 		print "yes"
 		break
+	
+	l3 = l.encode('ascii', 'replace')
 	print l3
 
 
