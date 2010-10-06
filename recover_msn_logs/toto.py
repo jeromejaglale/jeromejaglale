f3fcd6040668c83bebb5ded9d7f953394370f9bb
jerome.jaglale <jerome.jaglale@c5f109f2-20d2-11df-a5a4-5bcf8f6a2e7e>
2010-10-06 17:48:45 +0000

    bug fix

--- recover_msn_logs/toto.py
+++ recover_msn_logs/toto.py
@@ -3,7 +3,7 @@
 import codecs
 
 path = '11:20:03@0'
-path = '10:14:04@0'
+#path = '10:14:04@0'
 
 s=""
 f = codecs.open(path, 'rU', encoding='latin1')
@@ -14,7 +14,7 @@ for l in s.splitlines(True):
 	l3 = l.encode('utf-8', 'replace')
 	l3 = l3.strip()
 	if 'runs' in l3:
-		t = s.split('runs')
+		t = l3.split('runs')
 		print t[0]
 		break;
 	
