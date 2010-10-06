b051399c15e3d609c5c53effddea6b2d15f83295
jerome.jaglale <jerome.jaglale@c5f109f2-20d2-11df-a5a4-5bcf8f6a2e7e>
2010-10-06 19:09:41 +0000

    handle multiple files

--- recover_msn_logs/toto.py
+++ recover_msn_logs/toto.py
@@ -3,32 +3,30 @@
 import sys
 import codecs
 
-path = sys.argv[1]
+for path in sys.argv[1:]:
+	f = codecs.open(path, 'rU', encoding='latin1')
+	s=""
+	for l in f:
+		s += l
 
-f = codecs.open(path, 'rU', encoding='latin1')
-s=""
-for l in f:
-	s += l
-
-i = 0
-for l in s.splitlines(True):
-	i += 1
-	l3 = l.encode('utf-8', 'ignore')
+	i = 0
+	for l in s.splitlines(True):
+		i += 1
+		l3 = l.encode('utf-8', 'ignore')
 	
-	# first line
-	if i == 1:
-		print l3[24:]
-		continue
+		# first line
+		if i == 1:
+			continue
 	
-	# last line
-	sep = 'runs' + chr(0)
-	if sep in l3:
-		t = l3.split(sep)
-		print t[0]
-		break;
+		# last line
+		sep = 'runs' + chr(0)
+		if sep in l3:
+			t = l3.split(sep)
+			print t[0]
+			break;
 	
-	# normal line
-	l3 = l3.strip()
-	if l3 != '':
-		print l3
+		# normal line
+		l3 = l3.strip()
+		if l3 != '':
+			print l3
 
