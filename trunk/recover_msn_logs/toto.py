1266f44f16329e2362557dcdfb8213f94e48ec52
jerome.jaglale <jerome.jaglale@c5f109f2-20d2-11df-a5a4-5bcf8f6a2e7e>
2010-10-06 17:53:34 +0000

    seems to work now!

--- trunk/recover_msn_logs/toto.py
+++ trunk/recover_msn_logs/toto.py
@@ -2,22 +2,32 @@
 
 import codecs
 
-path = '11:20:03@0'
-#path = '10:14:04@0'
+path = '10:14:04@0'
 
 s=""
 f = codecs.open(path, 'rU', encoding='latin1')
 for l in f:
 	s += l
 
+i = 0
 for l in s.splitlines(True):
+	i += 1
 	l3 = l.encode('utf-8', 'replace')
 	l3 = l3.strip()
+	
+	# first line
+	if i == 1:
+		t = l3.split('"')
+		print t[1].strip()
+		continue;
+	
+	# last line
 	if 'runs' in l3:
 		t = l3.split('runs')
 		print t[0]
 		break;
 	
+	# normal line
 	if l3 != '':
 		print l3
 
