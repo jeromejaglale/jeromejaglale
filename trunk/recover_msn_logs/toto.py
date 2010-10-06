dacfcf3b778429c2ec353cbfc591e932f334a56e
jerome.jaglale <jerome.jaglale@c5f109f2-20d2-11df-a5a4-5bcf8f6a2e7e>
2010-10-06 23:54:43 +0000

    back to text, simpler

--- trunk/recover_msn_logs/toto.py
+++ trunk/recover_msn_logs/toto.py
@@ -16,8 +16,8 @@ for path in sys.argv[1:]:
 	output=""
 	for l in s.splitlines(True):
 		i += 1
-		l3 = l.encode('utf-8', 'ignore')
-	
+		l3 = l
+		
 		# first line
 		if i == 1:
 			continue
@@ -44,27 +44,12 @@ for path in sys.argv[1:]:
 	month = re.sub(reg, r'\1', filename).zfill(2) 
 	day = re.sub(reg, r'\2', filename).zfill(2) 
 	
-	new_filename = year + '_' + month + '_' + day + '.html'
+	new_filename = year + '_' + month + '_' + day + '.txt'
 	new_path = os.path.join(dirname, new_filename)
-	
-	
-	output = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-</head>
-
-<body>
-%s
-</body>
-</html>
-""" % output
-	
-	print output
-	f2 = codecs.open(new_path, 'w', encoding='utf-8')
-	output = output.decode('utf-8', 'ignore')
+		
+	print type(output)
+	f2 = codecs.open(new_path, 'w', encoding='utf-8-sig')
 	f2.write(output)
-	print "wrote into " + new_path
 	f2.close()
+	print "wrote into " + new_path
 
