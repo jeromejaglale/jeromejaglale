576608eea855bc9c7c9c8af6cfa2e2c15ddfe4b2
jeromejaglale <jeromejaglale@c5f109f2-20d2-11df-a5a4-5bcf8f6a2e7e>
2012-05-10 21:16:10 +0000

    update version number

--- trunk/codeigniter/i18n_library/MY_Lang.php
+++ trunk/codeigniter/i18n_library/MY_Lang.php
@@ -2,7 +2,7 @@
 
 // CodeIgniter i18n library by Jérôme Jaglale
 // http://maestric.com/en/doc/php/codeigniter_i18n
-// version 9 - April 2, 2012
+// version 10 - May 10, 2012
 
 class MY_Lang extends CI_Lang {
 
