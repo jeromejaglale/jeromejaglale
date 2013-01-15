6dbff4165d8e788e7834c9961affc5fb681a4ed1
jerome.jaglale@gmail.com <jerome.jaglale@gmail.com@c5f109f2-20d2-11df-a5a4-5bcf8f6a2e7e>
2013-01-15 21:22:39 +0000

    cleanup

--- trunk/jsp_text_to_properties/jsp_text_to_properties.py
+++ trunk/jsp_text_to_properties/jsp_text_to_properties.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 
-import re
-
+# constants
+##################################################
 KEY_PREFIX = 'admin.'
 KEY_MAX_LENGTH = 20
 
-props = {}
+# functions
+##################################################
+def get_text(l):
+	#print l
+	l = re.sub('<[^<]+?>', '', l) # remove HTML/XML tags: <...>
+	l = re.sub('<[^<]+?>', '', l)
+	l = re.sub('\${[^{}]+?}', '', l) # remove EL: ${...}
+	l = l.strip()
+	return l
 
 def get_key(text):
     key = text.lower()
@@ -22,33 +30,35 @@ def get_key(text):
     key = key.strip()
     key = key.replace(" ", "_")
     key = key[:KEY_MAX_LENGTH]
-    key = KEY_PREFIX + key
+    key = key
     return key
+
+# add prefix and number suffix to key
+def get_final_key(key, props):
+	final_key = KEY_PREFIX + key
+	i = 2
+	while(final_key in props.keys()):
+		final_key = KEY_PREFIX + key + str(i)
+		i+=1
+	return final_key
+
+# main
+##################################################
+import re, os
+
+props = {}
 
 path = '/home/jerome/projects/maestric/shared/jeromejaglale/jsp_text_to_properties/group-view.jsp'
 with open(path) as f:
+	is_first_key = True
 	for l in f:
-		#print l
-		l = re.sub('<[^<]+?>', '', l) # remove HTML/XML tags: <...>
-		l = re.sub('<[^<]+?>', '', l)
-		l = re.sub('\${[^{}]+?}', '', l) # remove EL: ${...}
-		l = l.strip()
-		if l:
-			# if string is already in props
-			if(l in props.values()):
-				continue
-			#print l
-			
-			key = get_key(l)
-
-			# if key already exists
-			final_key = key
-			i = 2
-			while(final_key in props.keys()):
-				final_key = key + str(i)
-				i+=1
-
-			#print final_key
-			props[final_key] = l
-
-			print final_key + '=' + l
+		text = get_text(l)
+		#print text			
+		if text and not text in props.values():
+			key = get_key(text)
+			final_key = get_final_key(key, props)
+			props[final_key] = text
+			if is_first_key:
+				print "# " + os.path.basename(path)
+				is_first_key = False
+			print final_key + '=' + text
