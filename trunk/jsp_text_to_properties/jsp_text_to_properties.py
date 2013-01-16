32437c03a5849405fdb039151c1518a1ff922ff7
jerome.jaglale@gmail.com <jerome.jaglale@gmail.com@c5f109f2-20d2-11df-a5a4-5bcf8f6a2e7e>
2013-01-16 20:54:47 +0000

    process folder

--- trunk/jsp_text_to_properties/jsp_text_to_properties.py
+++ trunk/jsp_text_to_properties/jsp_text_to_properties.py
@@ -3,8 +3,11 @@
 # constants
 ##################################################
 MESSAGE_TAG = """<spring:message code="%s" />"""
+
 KEY_PREFIX = 'admin.'
 KEY_MAX_LENGTH = 20
+
+FILE_EXTENSIONS = '.jsp,'
 
 # functions
 ##################################################
@@ -17,22 +20,22 @@ def get_text(l):
 	return l
 
 def get_key(text):
-    key = text.lower()
+	key = text.lower()
 
-    # replace special chars by single space 
-    special_chars = """~`!@#$%^&*()-_+=|\{}[]"':;,.<>/?\t"""
-    for i in range(len(special_chars)):
-    	key = key.replace(special_chars[i], " ")
+	# replace special chars by single space 
+	special_chars = """~`!@#$%^&*()-_+=|\{}[]"':;,.<>/?\t"""
+	for i in range(len(special_chars)):
+		key = key.replace(special_chars[i], " ")
 
-    # replace double spaces by single space
-    key = key.replace("  ", " ")
-    key = key.replace("  ", " ")    
+	# replace double spaces by single space
+	key = key.replace("  ", " ")
+	key = key.replace("  ", " ")    
 
-    key = key.strip()
-    key = key.replace(" ", "_")
-    key = key[:KEY_MAX_LENGTH]
-    key = key
-    return key
+	key = key.strip()
+	key = key.replace(" ", "_")
+	key = key[:KEY_MAX_LENGTH]
+	key = key
+	return key
 
 # add prefix and number suffix to key
 def get_final_key(key, props):
@@ -48,6 +51,33 @@ def get_new_line(l, text, key):
 	l_out = l.replace(text, tag)
 	return l_out
 
+def process_file(path):
+	with open(path) as f:
+		# open tmp file
+		tmp_path = path + ".tmp"
+		f_out = open(tmp_path, "w")
+
+		is_first_key = True
+		for l in f:
+			text = get_text(l)
+			if text:
+				if text in props.values():
+					final_key = [k for k, v in props.iteritems() if v == text][0]
+				else:
+					key = get_key(text)
+					final_key = get_final_key(key, props)
+					props[final_key] = text
+					if is_first_key:
+						print "# " + os.path.basename(path)
+						is_first_key = False
+					print final_key + '=' + text
+				l_out = get_new_line(l, text, final_key)
+				f_out.write(l_out)
+			else:
+				 f_out.write(l)
+
+		f_out.close()
+		#os.rename(tmp_path, path)
 
 # main
 ##################################################
@@ -55,30 +85,11 @@ import re, os
 
 props = {}
 
-path = '/home/jerome/projects/maestric/shared/jeromejaglale/jsp_text_to_properties/group-view.jsp'
-with open(path) as f:
-	# open tmp file
-	tmp_path = path + ".tmp"
-	f_out = open(tmp_path, "w")
-
-	is_first_key = True
-	for l in f:
-		text = get_text(l)
-		if text:
-			if text in props.values():
-				final_key = [k for k, v in props.iteritems() if v == text][0]
-			else:
-				key = get_key(text)
-				final_key = get_final_key(key, props)
-				props[final_key] = text
-				if is_first_key:
-					print "# " + os.path.basename(path)
-					is_first_key = False
-				print final_key + '=' + text
-			l_out = get_new_line(l, text, final_key)
-			f_out.write(l_out)
-		else:
-			f_out.write(l)
-
-	f_out.close()
-	#os.rename(tmp_path, path)
+#path = '/home/jerome/projects/maestric/shared/jeromejaglale/jsp_text_to_properties/group-view.jsp'
+dir_name = '/home/jerome/projects/maestric/shared/jeromejaglale/jsp_text_to_properties'
+for dirpath, dirnames, filenames in os.walk(dir_name):
+	for fname in filenames:
+		fext = os.path.splitext(fname)[1]
+		if fext != '' and fext in FILE_EXTENSIONS.split(','):
+			path = os.path.join(dirpath, fname)
+			process_file(path)
