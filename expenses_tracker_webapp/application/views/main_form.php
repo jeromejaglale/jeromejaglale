c99ef5bb827fcec106e5166fb19aa2e121957740
jerome.jaglale <jerome.jaglale@gmail.com>
2013-05-22 23:47:05 +0000

    simple form

--- expenses_tracker_webapp/application/views/main_form.php
+++ expenses_tracker_webapp/application/views/main_form.php
@@ -3,16 +3,28 @@
 <head>
 	<meta charset="utf-8">
 	<title>Expenses Tracker</title>
+
+	<link rel="stylesheet" href="<?=base_url()?>css/main.css?v=1" />
+
+	<script src="<?=base_url()?>js/jquery.js"></script>
+	<script src="<?=base_url()?>js/main.js?v=1"></script>
 </head>
+
 <body>
 <div class="container">
 	<form method="post" action="">
-		<link rel="stylesheet" href="<?=base_url()?>css/main.css?v=1" />
+			<p>
+				Amount<br />
+				<input type="number" name="amount" id="amount"/>
+		 	</p>
+			<p>
+				Note<br />
+				<input type="text" name="note" id="note"/>
+		 	</p>
+ 	</div>
+</fieldset>
 
-		<script src="<?=base_url()?>js/jquery.js"></script>
-		<script src="<?=base_url()?>js/main.js?v=1"></script>
 	</form>
 </div>
-
 </body>
 </html>
