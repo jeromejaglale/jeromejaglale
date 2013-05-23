46793bd3bd3fd3570cef612be922c25df0c46bef
jerome.jaglale@gmail.com <jerome.jaglale@gmail.com@c5f109f2-20d2-11df-a5a4-5bcf8f6a2e7e>
2013-05-23 17:59:53 +0000

    get date

--- trunk/expenses_tracker_webapp/js/main.js
+++ trunk/expenses_tracker_webapp/js/main.js
@@ -6,6 +6,12 @@ function l(str) {
 
 $(document).ready(function (){
 	$('form').submit(function(){
+		var amount = $('#amount', $(this)).val(),
+			note = $('#note', $(this)).val(),
+			d = new Date(),
+			date = d.getFullYear() + '-' + d.getMonth() + '-' + d.getDay() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+
+		l(date);
 		return false;
 	});
 });
