44865a7e9232e4c6cb0b942d4a67145fcff24da3
jerome.jaglale@gmail.com <jerome.jaglale@gmail.com@c5f109f2-20d2-11df-a5a4-5bcf8f6a2e7e>
2013-09-24 18:07:53 +0000

    add navs

---
+++ trunk/responsive/application/views/tmp.php
@@ -0,0 +1,54 @@
+
+	<div class="row">
+		<div class="large-12 columns">
+<nav class="top-bar">
+	<ul class="title-area">
+	<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+	<li class="toggle-topbar menu-icon"><a href=""><span>Menu</span></a></li>
+	</ul>
+
+	<section class="top-bar-section">
+
+	<ul class="right">
+	  <li>
+	  	<a href="#">About</a>
+	  </li>
+	  <li>
+	    <a href="">Partners &amp; Contributors</a>
+	  </li>              
+	  <li>
+	    <a href="">Contact</a>
+	  </li>              
+	</ul>
+	</section>
+</nav>
+</div>
+</div>
+
+	<div class="row">
+		<div class="large-12 columns">
+			<nav class="top-bar">
+				<ul class="title-area">
+	            <li class="name">
+	            </li>
+				<li class="toggle-topbar menu-icon"><a href=""><span><!-- Menu --></span></a></li>
+				</ul>
+
+				<section class="top-bar-section">
+
+				<ul class="left">
+				  <li>
+				  	<a href="#">Home</a>
+				  </li>
+				  <li>
+				    <a href="">Participate</a>
+				  </li>              
+				  <li>
+				    <a href="">Search</a>
+				  </li>              
+				</ul>
+				</section>
+			</nav>
+		</div>
+	</div>
+
